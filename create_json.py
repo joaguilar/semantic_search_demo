f81f45b0fec22835908f7589dee4128b6b1374cd
I_JeanCa <jeancarlo@bytescasino.com>
2023-04-25 20:34:05 -0600

    changes

--- create_json.py
+++ create_json.py
@@ -3,64 +3,56 @@ import tempfile
 import json
 
 txt_files=[]
-
+json_files=[]
 
 
 for file in os.scandir(".\\data\\documents\\."):
-    print(file.path)
+    #print(file.path)
     if (file.path.endswith("txt")):
-        print(file.path)
         print("---------------------------------------------------------------------")
         txt_files.append(file)
-        print(txt_files)
+        print("---------------------------------------------------------------------")
+        print("---------------------------------------------------------------------")
+
+for file in os.scandir(".\\data\\jsons\\."):
+    #print(file.path)
+    if (file.path.endswith("json")):
+        print("---------------------------------------------------------------------")
+        json_files.append(file)
         print("---------------------------------------------------------------------")
         print("---------------------------------------------------------------------")
 
 
 i=0
 
-for txt in txt_files:
-    print("Processing txt =  "+txt.path)
-    with open(txt.path, 'r', encoding="utf8") as f:
-        lines = f.readlines()
-        
-    test =" \ "
-    test2= test.strip()+"n"
-    lines = [line.replace('\n', test2) for line in lines]
-    
-    if(i==3):
-        i+=1 
-    print("---------------------------------------------------------------------")
-    print("Contador en "+str(i))
-    print("---------------------------------------------------------------------")
 
-       
-    print("Creado txt nuevo docuemnto"+str(i))
-    print("---------------------------------------------------------------------")
-    with open(".\data\documents\Texto Json\documento"+str(i)+".txt", 'w', encoding="utf8") as f:
-        f.writelines(lines)
 
+
+
+  
     
-    print("Abriendo JSON documento"+str(i))
+for json_Data in json_files:
+    data={}
+
     print("---------------------------------------------------------------------")
-    with open(".\data\jsons\.\documento"+str(i)+".json",'r', encoding="utf8") as file:
+    with open(json_Data.path,'r', encoding="utf8") as file:
+        print("Load Json: "+ str(file))
         data = json.load(file)
     
-    print("Sobreescribiendo JSON con el txt documento"+str(i))
-    print("---------------------------------------------------------------------")
-    with open(txt.path, 'r', encoding="utf8") as f:
-        data['content']=f.read()
-        #print("JSON")
-        #print(data)
+        print("Processing"+ json_Data.path)
+        print("---------------------------------------------------------------------")
 
+        test= json_Data.path.replace(".json",".txt").replace("jsons","documents\\")
+        print("Ruta del txt: " + test)
+        print("Ruta del json: " + json_Data.path)
 
+        with open(test, 'r', encoding="utf8") as f:
+            data['content']=f.read()
     
-    print("Update Json document"+str(i))
-    with open(".\data\jsons\.\documento"+str(i)+".json",'w', encoding="utf8") as jsonUpdate:
-            json.dump(data,jsonUpdate,ensure_ascii= False)
+    print("Update Json document"+ json_Data.path)
+    with open(json_Data.path,'w', encoding="utf8") as jsonUpdate:
+        json.dump(data,jsonUpdate,ensure_ascii= False)
     
-    
-    i+=1
     
     
     
